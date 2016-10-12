f8ad9faea8d46391c0336c34cc83bf5558eea1e9
Rohan Taneja <rohan@Administrators-MacBook-Air.local>
2016-10-12 12:54:49 -0400

    dogerg

---
+++ firstPHP.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>What up php?</title>
+</head>
+<body>
+	<?php
+		echo("<input id = 'toOutput'>");
+		echo("hello");
+		
+	?>
+	<button id="changeStuff"></button>
+	<div id ="outputDiv"></div>
+
+</body>
+</html>
