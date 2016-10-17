756c8be696a5015be42664a002ad3624a1dd5e7f
Rohan Taneja <rohan@AdminisatorsAir.fios-router.home>
2016-10-16 21:38:32 -0400

    doheroku

--- secondPHP.php
+++ secondPHP.php
@@ -1,9 +1,10 @@
 <?php 
 		$num = $_REQUEST["numIn"];
+		$isPrime = "true";
 		for($x=2; x<$num; $x++)
 		{
 			if($num%$x ==0)
-					$isPrime=false;
+					$isPrime="false";
 		}
 		echo $num;
 		echo("<br>".$isPrime);
