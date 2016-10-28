625b6b6e56b0cc23bcced896c1621fc1f3235d09
Rohan Taneja <rohan@Administrators-MacBook-Air.local>
2016-10-28 14:06:48 -0400

    doheroku

--- words.php
+++ words.php
@@ -1,6 +1,15 @@
 <?php
+//$s = $_REQUEST["str"];
+
 //$x = $_GET("str");
 $myfile = fopen("words.txt", "r") or die("Unable to open file!");
-echo fread($myfile, filesize("words.txt"));
+$file = fread($myfile, filesize("words.txt"));
+$lines = file("words.txt" FILE_IGNORE_NEW_LINES);
+echo($lines);
+// for($i =0; $i<filesize("words.txt");$i++)
+// {
+// 	if($file contains )
+// }
+//echo fread($myfile, filesize("words.txt"));
 fclose($myfile);
 ?>
