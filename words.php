bfb5ec035c68ec7780a9a282ecffbc2a35a3c06a
Rohan Taneja <rohan@Administrators-MacBook-Air.local>
2016-11-02 13:34:31 -0400

    doheroku

--- words.php
+++ words.php
@@ -1,24 +1,32 @@
 <?php
 $s = $_REQUEST["str"];
+// $s = $argv[1];
 
 
 //$x = $_GET("str");
 $myfile = fopen("words.txt", "r") or die("Unable to open file!");
-//echo fread($myfile, filesize("words.txt"));
+$f = fread($myfile, filesize("words.txt"));
 // $file = file("words.txt", FILE_IGNORE_NEW_LINES);
 // echo($file);
 $homepage = file_get_contents('words.txt');
-$pieces = explode(" ", $homepage);
-$bool = false;
-for($i = 0; $i < sizeof($pieces); $i++)
-{
-if($pieces[$i] == $s)
-	{
-		$bool = true;
-		echo($s);
-	}
-}
-// if($bool == true)
+$reg = "/".$s."/im";
+//echo($reg);
+$num = preg_match_all($reg, $f, $match);
+echo($num);
+
+
+
+// $pieces = explode(" ", $homepage);
+// echo("Size is ");
+// echo(sizeof($pieces));
+// if($pieces[$i] == $s)
+// 	{
+// 		$bool = true;
+// 		echo($s);
+// 	}
+	
+// }
+// // if($bool == true)
 // 	echo($s);
 // else
 // 	echo("Not A Real Word");
