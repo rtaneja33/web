60d5cbc0a4a1813638304a80d57be43c28af451a
Rohan Taneja <rohan@Administrators-MacBook-Air.local>
2016-11-16 13:52:03 -0500

    doheroku

--- Nov16.php
+++ Nov16.php
@@ -1,6 +1,12 @@
 <?php 
+	$host="host=ec2-174-129-223-35.compute-1.amazonaws.com";
+	$dbname="dbname=d7b1p5egvpv1r1";
+	$user="user=ixmcxtritsaljf";
+	$port="port=5432";
+	$password="password=1_NfGYcoaASkseEh0zvQ_eOffl";
+	$db = pg_connect($host." " .$dbname." " .$user." " .$port." " .$password);
 	//create an insert query
-	<<<CREATE TABLE FOOTBALL
+	$table = <<<CREATE TABLE FOOTBALL
 (
 PersonID int,
 LastName varchar(255),
@@ -9,6 +15,14 @@ Address varchar(255),
 City varchar(255)
 );
 CREATE;
+$ret = pg_query($query);
+	if(!$ret)
+	{
+		echo(pg_last_error($db));
+	}
+	else{
+		echo("IT WORKED!");
+	}
 	$queryStr= <<<QUERY
 	INSERT INTO FOOTBALL VALUES("Bill Belicheck","Patriots",7,2,0)
 
