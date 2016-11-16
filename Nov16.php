e7127b6055ea4812497d65992afc523366ce2400
Rohan Taneja <rohan@Administrators-MacBook-Air.local>
2016-11-16 14:11:40 -0500

    doheroku

--- Nov16.php
+++ Nov16.php
@@ -6,35 +6,35 @@
 	$password="password=1_NfGYcoaASkseEh0zvQ_eOffl";
 	$db = pg_connect($host." " .$dbname." " .$user." " .$port." " .$password);
 	//create an insert query
-	$table = <<<CREATE TABLE FOOTBALL
-(
-PersonID varchar(255),
-LastName varchar(255),
-Address int,
-City int,
-SomethingElse int
-);
-CREATE;
-$ret = pg_query($query);
-	if(!$db)
-	{
-		echo(pg_last_error($db));
-	}
-	else{
-		echo("IT WORKED!");
-	}
-	$queryStr= <<<QUERY
-	INSERT INTO FOOTBALL VALUES("Bill Belicheck","Patriots",7,2,0)
+// 	$table = <<<CREATE TABLE FOOTBALL
+// (
+// PersonID varchar(255),
+// LastName varchar(255),
+// Address int,
+// City int,
+// SomethingElse int
+// );
+// CREATE;
+// $ret = pg_query($query);
+// 	if(!$db)
+// 	{
+// 		echo(pg_last_error($db));
+// 	}
+// 	else{
+// 		echo("IT WORKED!");
+// 	}
+// 	$queryStr= <<<QUERY
+// 	INSERT INTO FOOTBALL VALUES("Bill Belicheck","Patriots",7,2,0)
 
-QUERY;
-	pg_query()($queryStr);
+// QUERY;
+// 	pg_query()($queryStr);
 
-		$queryStr= <<<QUERY
-	SELECT * FROM FOOTBALL
+// 		$queryStr= <<<QUERY
+// 	SELECT * FROM FOOTBALL
 
-QUERY;
-	$output = pg_query()($queryStr);
-	while($row=pg_fetch_assoc($output))
-		var_dump($row);
+// QUERY;
+// 	$output = pg_query()($queryStr);
+// 	while($row=pg_fetch_assoc($output))
+// 		var_dump($row);
 
  ?>
