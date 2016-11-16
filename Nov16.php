29e00b5b2a8001db3c5c51309f84e7e74dd17398
Rohan Taneja <rohan@Administrators-MacBook-Air.local>
2016-11-16 13:57:58 -0500

    doheroku

--- Nov16.php
+++ Nov16.php
@@ -16,13 +16,13 @@ City varchar(255)
 );
 CREATE;
 // $ret = pg_query($query);
-// 	if(!$ret)
-// 	{
-// 		echo(pg_last_error($db));
-// 	}
-// 	else{
-// 		echo("IT WORKED!");
-// 	}
+	if(!$db)
+	{
+		echo(pg_last_error($db));
+	}
+	else{
+		echo("IT WORKED!");
+	}
 	$queryStr= <<<QUERY
 	INSERT INTO FOOTBALL VALUES("Bill Belicheck","Patriots",7,2,0)
 
